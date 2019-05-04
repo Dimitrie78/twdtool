c236bb287d9aac458c22d63b76e32f87ed2b3812
Flo <49324285+Overkill82@users.noreply.github.com>
2019-05-04 22:39:48 +0200

    Edit, Add, Style
    Hinzufügen, ändern und das Styling angepasst. Code gesäubert. Todo: Löschen von Gruppen.

--- inc/groupedit.php
+++ inc/groupedit.php
@@ -6,19 +6,10 @@ msgbox($_GET['msg']);
 if (!isset($_GET['do'])){
 $grpqry = $pdo->query('SELECT id,tag,name FROM '.$config->db_pre.'groups ORDER BY name');
 $grpqry->execute();
-$tbody = '';
-
-foreach ($grpqry as $group) {
-	$tbody .=  '<tr>
-		  <td style="text-align: left;">'.$group['id'].'</td>
-		  <td style="text-align: left;">'.$group['tag'].'</td>
- 		  <td style="text-align: left;">'.$group['name'].'</td>
-		   <td style="text-align: left;"><a href = "?action=groupedit&do='.$group['id'].'">EDIT</a></td>
-		</tr>';
-}
-	
-$thead = '
-<div class="table-responsive"> <table class="table table-hover table-fixed datatable table-bordered" id="sortTable" style="width:auto">
+?>
+<a href="?action=groupedit&do=add" style = "margin-bottom: 10px" class="btn btn-success" role="button"><span class = "fas fa-plus-square"></span> Gruppe erstellen</a>
+<div class="table-responsive">
+<table class="table table-hover table-fixed datatable table-bordered" id="sortTable" style="width:auto">
   <thead class="thead-dark">
     <tr>
 	  <th scope="col" style="min-width: 41px;">ID</th>
@@ -27,27 +18,96 @@ $thead = '
 	  <th scope="col" style="min-width: 50px;">Edit</th>
     </tr>
   </thead>
-  <tbody>';
-
+  <tbody>
+<?php
+foreach ($grpqry as $group) {
+?>
+	<tr>
+	  <td><?php echo $group['id']; ?></td>
+	  <td><?php echo $group['tag']; ?></td>
+      <td><?php echo $group['name']; ?></td>
+	  <td><a href = "?action=groupedit&do=update&id=<?php echo $group['id']; ?>" class="btn btn-primary btn-xs"><span class = "fas fa-edit"></span> EDIT</a></td>
+	</tr>
+<?php
+}
+?>
+ </tbody>
+</table>
+</div>
+<?
+}
 	
-$tfoot = '</tbody>
-</table></div>';
-
-echo $thead.$tbody.$tfoot;
+if($_GET['do'] == "update" && is_numeric($_GET['id'])){
+	$grpqry = $pdo->prepare("SELECT id,tag,name FROM ".$config->db_pre."groups WHERE id = :id");
+	$grpqry->execute(array('id' => $_GET['id']));
+	if($group = $grpqry->fetch()){?>
+<span style = "display: inline-block; margin-bottom:10px; font-weight:bold;">Gruppen-ID: <?php echo $group['id'];?></span>
+		<form action="index.php?action=groupedit&do=update2db" method="POST" autocomplete="no">
+         <input type="hidden" name="editid" value = "<?php echo $group['id'];?>">
+		  <div class="form-group">
+			<label for="grouptag">Tag/Kürzel:</label>
+			<input type="text" class="form-control" id="grouptag" name="grouptag" 
+			value = "<?php echo $group['tag'];?>" maxlength = "3" required>
+		  </div>
+		  <div class="form-group">
+			<label for="groupname">Gruppen-Name:</label>
+			<input type="text" class="form-control" id="groupname" name = "groupname"
+			value = "<?php echo $group['name'];?>" maxlength = "16" required> 
+		  </div>
+		  <div class="clearfix">
+		   <div class="pull-left">
+		    <button type="submit" name = "update" value="UpdateGroup" class="btn btn-success">
+		    <span class = "fas fa-edit"></span> Gruppe updaten</button>
+	       </div>
+		   <div class="pull-right">
+		  <a href="?action=groupedit&do=delete&id=<?php echo $group['id'];?>" class="btn btn-danger" role="button">
+            <span class = "fas fa-trash-alt"></span> Gruppe löschen</a>
+		    <a href="?action=groupedit" class="btn btn-warning" role="button">
+		    <span class = "fas fa-step-backward"></span> zurück</a><br>
+		   </div>
+		  </div>
+		</form>
+<?php
 	}
+	else {
+		failmsg('Der Eintrag konnte nicht geladen werden!');
+	}
+}
 
-if($_GET['do'] == "add"){
+if($_POST['update'] == "UpdateGroup" && is_numeric($_POST['editid'])){
+##todo: validiation
+	$query = $pdo->prepare('UPDATE '.$config->db_pre.'groups SET tag = :tag, name = :name WHERE id = :id');
+	if($query->execute(array(':tag' => $_POST['grouptag'],
+						 ':name' => $_POST['groupname'],
+						 ':id' => $_POST['editid']))){
+		header("Location: ?action=groupedit&do=update&id=".$_POST['editid']."&msg=updatesuccess");  
+	} else {
+		header("Location: ?action=groupedit&do=update&id=".$_POST['editid']."&msg=updatefail");  
+	}	
 	
-?>	<form action="index.php?action=groupedit&do=add2db" method="POST" autocomplete="no">
+}
+
+if($_GET['do'] == "add"){?>
+<span style = "display: inline-block; margin-bottom:10px; font-weight:bold;">Gruppe erstellen</span>
+<form action="index.php?action=groupedit&do=add2db" method="POST" autocomplete="no">
   <div class="form-group">
     <label for="grouptag">Tag/Kürzel:</label>
-    <input type="grouptag" class="form-control" id="grouptag" name="grouptag" maxlength = "3" required>
+    <input type="text" class="form-control" id="grouptag" name="grouptag" maxlength = "3" required>
   </div>
   <div class="form-group">
     <label for="groupname">Gruppen-Name:</label>
-    <input type="groupname" class="form-control" id="groupname" name = "groupname" maxlength = "16" required> 
+    <input type="text" class="form-control" id="groupname" name = "groupname" maxlength = "16" required> 
   </div>
-<button type="submit" name = "creategroup" value="AddGroup" class="btn btn-success">Gruppe anlegen</button>
+  <div class="clearfix">
+    <div class="pull-left">
+      <button type="submit" name = "creategroup" value="AddGroup" class="btn btn-success">
+	  <span class = "fas fa-plus-square"></span> Gruppe anlegen</button>
+    </div>
+    <div class="pull-right">
+      <a href="?action=groupedit" class="btn btn-warning" role="button">
+      <span class = "fas fa-step-backward"></span> zurück</a><br>
+    </div>
+  </div>
 </form>
 <?
 	}
