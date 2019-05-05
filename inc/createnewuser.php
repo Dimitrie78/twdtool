d5031c37f59553ff50777c8d602c049db578f6ee
Flo <49324285+Overkill82@users.noreply.github.com>
2019-05-05 15:57:29 +0200

    Multigruppenfähigkeit, Anpassung Rechte.
    +Die Erstellung neuer User ist jetzt komplett Multigruppenfähig. Devs können die Gruppe und alle Rechte direkt angeben. Die vorausgewählte Gruppe ist die des Dev´s. 
    +(Gruppen)Admins können nur bis zum Admin-Recht anlegen und die Gruppe ist die gleiche in der der (Gruppen)Admin ist.
    + Zusätzlich Code weiter bereinigt und vereinfacht.

--- inc/createnewuser.php
+++ inc/createnewuser.php
@@ -1,7 +1,6 @@
 <?php 
-$allowed = "Dev";
 include "verify.php";
-
+	
 if(!isset($_POST['createuser'])) { ?>
 <form action="index.php?action=createnewuser" method="POST" autocomplete="no">
   <div class="form-group">
@@ -17,9 +16,34 @@ if(!isset($_POST['createuser'])) { ?>
 	<input autocomplete="new-password" type="password" class="form-control" id="pwd" name="pwd" value="" required>
   </div>
   
-<?php if (isadmin()){ ?>
+
+<?php
+if(isdev()){
+	?>
+	  <div class="form-group">
+		<label for="ugroup" class = "control-label">Gruppe:</label>
+		 <select  id="ugroup" name = "ugroup" class = "form-control">	    
+	<?php 
+	$grpqry = 'SELECT id,tag,name FROM '.$config->db_pre.'groups ORDER BY name';
+	foreach ($pdo->query($grpqry) as $group){
+		unset($selected);
+		if ($group['id'] == $_SESSION['gid']) {
+			$selected = ' selected';
+		}
+		 echo '<option value="'.$group['id'].'" '.$selected.'>['.$group['id'].'] ['.$group['tag'].'] '.$group['name'].'</option>';
+	}
+	?>
+		 </select>   
+	</div>
+	<?php
+}
+if (isadmin()){
+	if($_SESSION['role'] === 1){
+		array_pop($rights);
+	}
+?>
   <div class="form-group">
-	 <label for="inputUser" class = "control-label">Stattool-Rechte:</label>
+    <label for="inputUser" class = "control-label">Stattool-Rechte:</label>
      <select  id="inputUser" name = "role" class = "form-control">	    
 <?php 
 foreach($rights as $key => $value) {
@@ -27,7 +51,7 @@ foreach($rights as $key => $value) {
 }
 ?>
 	 </select>   
-	</div>
+  </div>
 <?php } ?>
 	
   <div class="form-group">
@@ -44,49 +68,39 @@ foreach($rights as $key => $value) {
 }
 
 if(isset($_POST['createuser']) && $_POST['createuser'] == "AddUser" & isadminormod()){
-
 	$curr_datetime =date("Y-m-d H:i:s");
 	$notetime = ($_POST['notes'] > "" ? $curr_datetime : ''); 
+	$groupid = ($_POST['ugroup'] > "" ? intval($_POST['ugroup']) : $_SESSION['gid']); 
 
 	if (isadmin()){
-		$statement = $pdo->prepare("INSERT INTO ".$config->db_pre."users(telegram, notes, notetime, ign, passwd, role, created_at, active)
-			VALUES(:telegram, :notes, :notetime, :ign, :passwd, :role, :created_at, :active)");
-
-
-		$statement->execute(array(
-			"telegram" => $_POST['telegram'],
-			"notes" => $_POST['notes'],
-			"notetime" => $notetime,
-			"ign" =>  $_POST['ign'],
-			"passwd" => password_hash($_POST['pwd'], PASSWORD_DEFAULT),
-			"role" => $_POST['role'],
-			"created_at" => $curr_datetime,
-			"active" => 1
-		));
+		$urole = $_POST['role'];
 	}
-
+	
 	if (ismod()){
-		$statement = $pdo->prepare("INSERT INTO ".$config->db_pre."users(telegram, notes, notetime, ign, passwd, role, created_at, active)
-			VALUES(:telegram, :notes, :notetime, :ign, :passwd, :role, :created_at, :active)");
+		$urole = 3;
+	}
+	
+		$statement = $pdo->prepare("INSERT INTO ".$config->db_pre."users(gid, telegram, notes, notetime, ign, passwd, role, created_at, active)
+			VALUES(:gid, :telegram, :notes, :notetime, :ign, :passwd, :role, :created_at, :active)");
 
 
 		$statement->execute(array(
+			"gid" => $groupid,
 			"telegram" => $_POST['telegram'],
 			"notes" => $_POST['notes'],
 			"notetime" => $notetime,
 			"ign" =>  $_POST['ign'],
 			"passwd" => password_hash($_POST['pwd'], PASSWORD_DEFAULT),
-			"role" => 3,
+			"role" => $urole,
 			"created_at" => $curr_datetime,
 			"active" => 1
 		));
-	}
 
-
-			
-	if($statement) {        
-		echo '<div class="alert alert-success"><strong>Nutzer '.$_POST['ign'].' angelegt!</strong> Weiterleitung...</div>';
-		header('Refresh: 2; URL=?action=usrmgr&uid='.$pdo->lastInsertId().'');
+	
+		$count = $statement->rowCount();
+		if($count =='1'){     
+			echo '<div class="alert alert-success"><strong>Nutzer '.$_POST['ign'].' angelegt!</strong> Weiterleitung...</div>';
+			header('Refresh: 2; URL=?action=usrmgr&uid='.$pdo->lastInsertId().'');
 	}
-}
+		}
 ?>
