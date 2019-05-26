a3b48a59cc1eb6981dfd5c48c6d31f39d78fd505
Overkill82 <49324285+Overkill82@users.noreply.github.com>
2019-05-26 15:17:45 +0200

    Übernimmt alte Info und berenigt config
    Vorhandene Clantag und Name wird direkt in die DB geschrieben, wenn vorhanden.
    Die config Datei wird danach automatisch angepasst.
    Die upgrade Datei entfernt sich selbst nach dem Prozess.

--- upgrade.php
+++ upgrade.php
@@ -1,19 +1,19 @@
 <?php
 ob_start();
 header("Content-Type: text/html; charset=utf-8");
-if (!file_exists("conf/config.php")){exit('Config Datei fehlt. Bitte installieren.');}
-$config = include("conf/config.php");
-$errmode = 'EXCEPTION'; // SILENT im Produktivbetrieb, EXCEPTION oder WARNING beim Debuggen
+$configfile = 'conf/config.php';
+if (!file_exists($configfile)){exit('Config Datei fehlt. Bitte installieren.');}
+$config = include($configfile);
+$errmode = 'EXCEPTION'; 
 define('TIMEZONE', 'Europe/Berlin');
 date_default_timezone_set(TIMEZONE);
-
 
 ?>
 
 <!DOCTYPE html>
 <html lang="de">
 <head>
-  <title>TWD Stattool: UPGRADE von 1.7</title>
+  <title>TWD Stattool Upgrader </title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://bootswatch.com/3/slate/bootstrap.min.css">
@@ -25,18 +25,46 @@ date_default_timezone_set(TIMEZONE);
 	<div class="modal-content">
 	  <div class="modal-heading">
 		<h2 class="text-center">TWD Stattool: UPGRADE von 1.7</h2>
+		<h4 class="text-center">Vorherige Sicherung von Datenbank und Dateien empfohlen</h3>
 	  </div>
-	  <hr />
+
 	  <div class="modal-body">
 <?php
-
 if ($_POST["do"] == "upgrade") {
+//Evtl. vorhandene Clan-Information aus alter Config auslesen
+$clantag = (isset($config->clantag)) ? $config->clantag : 'TWD';
+$clanname = (isset($config->clanname)) ? $config->clanname : 'twdclan';
+
+//Config neu schreiben
+if (is_writable($configfile)) {
+$string = '<?php 
+return (object) array(
+	\'dbhost\' => \''. $config->dbhost. '\',
+	\'dbusername\' => \''. $config->dbusername. '\',
+	\'dbpassword\' => \''. $config->dbpassword. '\',
+	\'dbname\' => \''. $config->dbname. '\',
+	\'ocrspace_apikey\' => \''. $config->ocrspace_apikey. '\',
+	\'theme\' => \''. $config->theme. '\',
+	\'statlimit\' => \''. $config->statlimit. '\',
+	\'db_pre\' => \''. $config->db_pre. '\');
+?>';
+
+$fp = FOPEN($configfile, "w");
+FWRITE($fp, $string);
+FCLOSE($fp);
+}
+else
+{
+echo "<br>Das bereinigen der Datei ".$configfile." ist fehlgeschlagen. Das Tool wird <b>dennoch funktionieren</b>. Optional können Sie die Zeilen mit clanname und clantag manuell entfernen. Sie werden nicht weiter benötigt.<br>";
+}
+		
 try {  
   $options = array(
         PDO::ATTR_ERRMODE => 'PDO::ERRMODE_'.$errmode, 
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
     );  
-  $pdo = new PDO("mysql:host=".$config->dbhost.";dbname=".$config->dbname.";charset=utf8", $config->dbusername, $config->dbpassword,$options);
+$pdo = new PDO("mysql:host=".$config->dbhost.";dbname=".$config->dbname.";charset=utf8", $config->dbusername, $config->dbpassword,$options);
+
 
 $sql = "CREATE TABLE IF NOT EXISTS `".$config->db_pre."groups` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
@@ -77,35 +105,13 @@ ALTER TABLE  `".$config->db_pre."news` CHANGE `active` `active` TINYINT( 1 ) NOT
 ALTER TABLE  `".$config->db_pre."users` ADD `gid` INT( 10 ) NOT NULL AFTER  `id` ;
 
 INSERT INTO `".$config->db_pre."groups` (`tag`, `name`) VALUES
-('TWD', 'Twdclan');
+('".$clantag."', '".$clanname."');
 
 UPDATE `".$config->db_pre."users` SET  `gid` =  '1';";
-
-			
-			if ($pdo->query($sql)) {
-				echo '<div class="alert alert-success">Daten erfolgreich in die Datebank geschrieben</div><br>';
-				if(file_exists('install.php') OR file_exists('upgrade.php') OR file_exists('config.php')){
-					echo 'Bitte löschen Sie jetzt aus Sicherheitsgründen die Installationsdateien sowie die alte Config-Datei, danach können Sie sich einloggen.<br><a href="upgrade.php?action=clean" class="btn btn-success btn-sm" role="button" >Installer löschen</a>';
-				} else {
-					echo '<a href="index.php" class="btn btn-success btn-sm" role="button">Login</a>';
-				}
-			} else {
-				echo '<div class="alert alert-warning">Fehler beim Datenbank import</div>';
-			}
-		} 
-
-
-	catch(PDOException $e){
-    echo "Datenbankverbindung fehlgeschlagen: " . $e->getMessage();
-    exit;
-		}
-	}
-
-
-if($_GET['action'] == "clean"){
-	$errdel = false;
-
-	if(file_exists('install.php')){
+	
+	if ($pdo->query($sql)) {		
+	  $errdel = false;
+	  if(file_exists('install.php')){
 		if(unlink('install.php'))
 		{
 			echo 'install.php entfernt!<br>';
@@ -115,36 +121,44 @@ if($_GET['action'] == "clean"){
 			echo 'install.php konnte nicht entfernt werden, bitte manuell löschen<br>';
 			$errdel = true;
 		}
-	}
-	if(file_exists('upgrade.php')){
-		if(unlink('upgrade.php'))
-		{
-			echo 'upgrade.php entfernt!<br>';
+	   }
+	   
+		if(file_exists('upgrade.php')){
+			if(unlink('upgrade.php'))
+			{
+				echo 'upgrade.php entfernt!<br>';
+			}
+			else
+			{
+				echo 'upgrade.php konnte nicht entfernt werden, bitte manuell löschen<br>';
+				$errdel = true;
+			}
 		}
-		else
+					
+		if ($errdel == false)
 		{
-			echo 'upgrade.php konnte nicht entfernt werden, bitte manuell löschen<br>';
-			$errdel = true;
+			echo '<div class="alert alert-success">Das Upgrade wurde erfolgreich durchgeführt.</div><br>
+			<a href="index.php" class="btn btn-success btn-sm" role="button" >Login</a>';
 		}
 	}
-
-	if ($errdel == false)
-	{
-		echo '<div class="alert alert-success">Alles OK Sie können sich jetzt einloggen. Alle Mitglieder wurden der Gruppe TWD zugewiesen. Diese kann im Script umbenannt werden.</div><br>
-		<a href="index.php" class="btn btn-success btn-sm" role="button" >Login</a>';
+	else {
+		echo '<div class="alert alert-warning">Fehler beim schreiben in die Datenbank. Bitte prüfen Sie die Zugangsdaten.</div>';
+		 }
+	} 
+	catch(PDOException $e){
+    echo "Datenbankverbindung fehlgeschlagen: " . $e->getMessage();
+    exit;
+		}
 	}
-}
-
 	if(!$_POST AND !$_GET) {
 	?>
-			<form action="" method = "POST" autocomplete="no" name="install" id="install">	  
-			  <div class="form-group text-center">
-			   <button type="submit" name="do" value="upgrade" class="btn btn-success">Upgrade starten</button>
-			  </div>
-			</form>		
+	<form action="upgrade.php" method = "POST" autocomplete="no" name="install" id="install">	  
+	  <div class="form-group text-center">
+	  <button type="submit" name="do" value="upgrade" class="btn btn-success">Upgrade starten</button>
+	 </div>
+	</form>		
 	<?php	
 	}
-
 ?>	
 	  </div>
 	</div>
