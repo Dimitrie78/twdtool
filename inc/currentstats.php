9730186a049fe2e9286de5dcfd8245c6081905be
Overkill82 <49324285+Overkill82@users.noreply.github.com>
2019-06-16 15:32:14 +0200

    Multigroupfähigkeit
    Currentstats jetzt Multigruppenfähig

--- inc/currentstats.php
+++ inc/currentstats.php
@@ -1,121 +1,127 @@
 <?php
 include "verify.php";
 
-$usrqry = $pdo->query('SELECT id,ign FROM '.$config->db_pre.'users WHERE active > 0 ORDER BY ign');
-$usrqry->execute();
-$tbody = '';
+echo "Aktuelle Statistik</b>";
 
-foreach ($usrqry as $usr) {
-	$uid = $usr['id'];
-	$uname = $usr['ign'];
-	
-	$sql1 = 'SELECT streuner, menschen, gespielte_missionen, abgeschlossene_missonen, gefeuerte_schuesse, haufen, heldenpower, waffenpower, karten, gerettete 
-		FROM  '.$config->db_pre.'stats
-		WHERE uid ='.$uid.'
-		ORDER BY date DESC 
-		LIMIT 0 , 1';
-
-	#reset variables
-	$streuner1 = '';
-	$menschen1 = '';
-	$gespielte_missionen1 = '';
-	$abgeschlossene_missonen1 = '';
-	$gefeuerte_schuesse1 = '';
-	$haufen1 = '';
-	$heldenpower1 = '';
-	$waffenpower1 = '';
-	$karten1 = '';
-	$gerettete1 = '';
-
-	foreach ($pdo->query($sql1) as $row1) {
-		$streuner1 = $row1['streuner'];
-		$menschen1 = $row1['menschen'];
-		$gespielte_missionen1 = $row1['gespielte_missionen'];
-		$abgeschlossene_missonen1 = $row1['abgeschlossene_missonen'];
-		$gefeuerte_schuesse1 = $row1['gefeuerte_schuesse'];
-		$haufen1 = $row1['haufen'];
-		$heldenpower1 = $row1['heldenpower'];
-		$waffenpower1 = $row1['waffenpower'];
-		$karten1 = $row1['karten'];
-		$gerettete1 = $row1['gerettete'];
-	}
-
-
-
-	$streuner = ($streuner1) ? $streuner1 : 0;
-	$menschen = ($menschen1) ? $menschen1 : 0;
-	$gespielte_missionen = ($gespielte_missionen1) ? $gespielte_missionen1  : 0;
-	$abgeschlossene_missonen = ($abgeschlossene_missonen1) ? $abgeschlossene_missonen1 : 0;
-	$gefeuerte_schuesse = ($gefeuerte_schuesse1) ? $gefeuerte_schuesse1  : 0;
-	$haufen = ($haufen1) ? $haufen1 : 0;
-	$heldenpower = ($heldenpower1) ? $heldenpower1 : 0;
-	$waffenpower = ($waffenpower1) ? $waffenpower1 : 0;
-	$karten = ($karten1) ? $karten1 : 0;
-	$gerettete = ($gerettete1) ? $gerettete1 : 0;
-
-
-	$tbody .=  '<tr>
-		  <td style="text-align: left; min-width: 120px;"><a href = "?action=stats&uid='.$uid.'">'.$uname.'</a></td>
-		  <td style="text-align: right;">'.number_format($streuner,0, ",", ".").'</td>
-		  <td style="text-align: right;">'.number_format($menschen,0, ",", ".").'</td>
-		  <td style="text-align: right;">'.number_format($gespielte_missionen,0, ",", ".").'</td>
-		  <td style="text-align: right;">'.number_format($abgeschlossene_missonen,0, ",", ".").'</td>
-		  <td style="text-align: right;">'.number_format($gefeuerte_schuesse,0, ",", ".").'</td>
-		  <td style="text-align: right;">'.number_format($haufen,0, ",", ".").'</td>					
-		  <td style="text-align: right;">'.number_format($heldenpower,0, ",", ".").'</td>		
-		  <td style="text-align: right;">'.number_format($waffenpower,0, ",", ".").'</td>	
-		  <td style="text-align: right;">'.number_format($karten,0, ",", ".").'</td>	
-		  <td style="text-align: right;">'.number_format($gerettete,0, ",", ".").'</td>	
-		</tr>';
-		
+if (!isdev()){
+$and_grouplimit = ' AND U.gid = '.$_SESSION['gid'];
 }
-	
-$thead = '
-<div class="table-responsive"> <table class="table table-hover table-fixed datatable table-bordered" id="sortTable" style="width:auto">
-  <thead class="thead-dark">
-    <tr>
-	  <th scope="col" style="min-width: 120px;">Spieler/in</th>
-      <th scope="col">Streuner</th>
-	  <th scope="col">Menschen</th>
-	  <th scope="col">Gesp. Mis.</th>
-	  <th scope="col">Abge. Mis.</th>
-      <th scope="col">Schüsse</th>
-      <th scope="col">Haufen</th>
-      <th scope="col">Helden</th>
-	  <th scope="col">Waffen</th>
-	  <th scope="col">Karten</th>
-	<th scope="col">Gerettet</th>
-    </tr>
-  </thead>
-  <tbody>';
 
-	
-$tfoot = '</tbody>
-</table></div>';
+if (isdev() && $_POST['gid']){
+if (is_numeric($_POST['gid'])){
+$and_grouplimit = ' AND U.gid = '.$_POST['gid'];
+}
 
-echo $thead.$tbody.$tfoot;
+if ($_POST['gid'] == "uc"){
+$and_grouplimit =  ' AND U.gid = 0';
+}
+}
+
+?>  
+<?php if (isdev()){?>
+<div class ="form-group">
+<form class="form-vertical" role="form" method = "POST" action = "?action=currentstats" >
+<label for="inputGroup" class = "control-label">Gruppe wählen: <span class="fas fa-arrow-right"></span></label>
+      <select onchange="this.form.submit()" id="inputGroup" name = "gid" class = "form-control" style="width:auto;min-width:200px;">
+	 <option value="allgrp" <?php if ($_POST['gid'] == 'allgrp'){echo ' selected';} ?>>--Alle--</option>
+	 <option value="uc" <?php if ($_POST['gid'] == 'uc'){echo ' selected';} ?>>--Ohne Gruppe--</option>
+<?php
+	$sql = 'SELECT id, tag, name FROM `'.$config->db_pre.'groups` ORDER BY name ASC';
 	
+    foreach ($pdo->query($sql) as $row) {
+		if ($_POST['gid'] == $row['id'])
+	{
+	$gidselected = ' selected';
+	}
+       echo '<option value="'.$row['id'].'" '.$gidselected.'>['.$row['tag'].'] '.$row['name'].'</option>';
+	    $gidselected = '';
+    }
+
 ?>
-<script>
+     </select>
+	 </div>
+<?php 
+}
 
-	// $(document).ready(function(){ $('#sortTable').tablesorter(); });
+$out = 'SELECT s1. * , U.gid, G.tag, U.ign
+FROM '.$config->db_pre.'stats AS s1
+RIGHT JOIN '.$config->db_pre.'users U ON s1.uid = U.id
+RIGHT JOIN '.$config->db_pre.'groups G ON G.id = U.gid
+LEFT JOIN (
+SELECT uid, MAX( DATE ) AS m
+FROM '.$config->db_pre.'stats
+GROUP BY uid
+) AS s2 ON s1.uid = s2.uid
+WHERE s1.date = s2.m
+AND s1.uid = s2.uid
+'.$and_grouplimit.'
+AND U.active =1
+ORDER BY G.tag ASC, U.ign ASC';
 
-	 $(document).ready(function () {
-        jQuery.tablesorter.addParser({
-            id: "fancyNumber",
-            is: function (s) {
-                return /^[0-9]?[0-9,\.]*$/.test(s);
-            },
-            format: function (s) {
-                return jQuery.tablesorter.formatFloat(s.replace(/\./g, ''));
-            },
-            type: "numeric"
-        });
 
-        $("#sortTable").tablesorter({
-            headers: { 0: { sorter: 'fancyNumber'} },
-            widgets: ['zebra']
-        });
-    }); 
-
-</script>
+$query = $pdo->prepare($out);
+$query->execute();
+$count = $query->rowCount();
+if ($count === 0){
+echo 'Es wurden noch keine Statistiken hochgeladen!';
+}
+else{
+?>   
+  <div class="table-responsive">
+  <table id="currentstats" class="table table-striped table-condensed datatable table-bordered nowrap table-hover table-fixed" style="width:100%">
+        <thead>
+			<tr>
+			<?php if (isdev()){ ?>
+				<th style="min-width: 50px;">GRP</th>
+			<?php }?>
+			    <th style="min-width: 120px;">IGN</th>
+                <th style="min-width: 100px;">DAT</th>
+                <th style="min-width: 120px;">EP</th>
+				<th>LVL</th>
+                <th>STR</th>
+				<th>MEN</th>
+                <th>GMIS</th>
+                <th>AMIS</th>
+                <th>SCHS</th>
+                <th>KST</th>
+				<th>HLD</th>
+				<th>WAF</th>
+				<th>KRT</th>
+			    <th>GRT</th>
+				<th>Edit</th>
+            </tr>
+        </thead>
+        <tbody>
+<?php
+foreach ($query as $row) {
+	echo '<tr>';
+				if (isdev()){
+				echo'
+				<td style="min-width: 50px; text-align: left;">'. $row['tag'].'</td>';
+				 }echo'
+		        <td style="min-width: 120px; text-align: left;">'.$row['ign'].'</td>
+                <td style="min-width: 100px; text-align: right;">'.date( 'd.m.y H:i:s', strtotime($row['date'])).'</td>
+				<td style="min-width: 120px; text-align: right;">'.$row['exp'].'</td>
+				<td style="text-align: right;">'. leveldata($row['exp']) .'</td>
+				<td style="text-align: right;">'.$row['streuner'].'</td>
+                <td style="text-align: right;">'.$row['menschen'].'</td>
+				<td style="text-align: right;">'.$row['gespielte_missionen'].'</td>
+                <td style="text-align: right;">'.$row['abgeschlossene_missonen'].'</td>
+				<td style="text-align: right;">'.$row['gefeuerte_schuesse'].'</td>
+                <td style="text-align: right;">'.$row['haufen'].'</td>
+				<td style="text-align: right;">'.$row['heldenpower'].'</td>
+                <td style="text-align: right;">'.$row['waffenpower'].'</td>
+				<td style="text-align: right;">'.$row['karten'].'</td>	
+				<td style="text-align: right;">'.$row['gerettete'].'</td>
+				<td style="text-align: center;">
+				<a href="?action=editstat&id='.$row['id'].'" role="button"><span class="fas fa-edit"></a>
+				</td>
+        </tr>';
+}
+?>
+	</tbody>
+	</table></div>
+<script>$(document).ready(function(){ $('#currentstats').tablesorter(); });</script>
+<?php
+}
+?>
